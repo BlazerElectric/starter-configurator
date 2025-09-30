69ecfc77f187237df10fd44cb28aeeea08c51b3d
BlazerElectric <dferguson@buyblazer.com>
2025-09-30 12:17:53 -0600

    Update StepSelector.js

--- src/components/StepSelector.js
+++ src/components/StepSelector.js
@@ -1,1 +1,20 @@
+import React from 'react';
 
+function StepSelector({ options, selection, onChange, stepKey }) {
+  return (
+    <div style={{ marginBottom: "1rem" }}>
+      <label style={{ marginRight: "0.5rem" }}>{stepKey}:</label>
+      <select
+        value={selection || ''}
+        onChange={e => onChange(stepKey, e.target.value)}
+      >
+        <option value="" disabled>Select...</option>
+        {options.choices.map(choice => (
+          <option key={choice} value={choice}>{choice}</option>
+        ))}
+      </select>
+    </div>
+  );
+}
+
+export default StepSelector;
