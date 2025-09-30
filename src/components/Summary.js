17afd38a6d2796aa04dafc4b3deb92985a228a9d
BlazerElectric <dferguson@buyblazer.com>
2025-09-30 12:18:19 -0600

    Update Summary.js

--- src/components/Summary.js
+++ src/components/Summary.js
@@ -1,1 +1,18 @@
+import React from 'react';
 
+function Summary({ sku, onEmail }) {
+  return (
+    <div style={{ marginTop: "2rem" }}>
+      {sku ? (
+        <>
+          <h2>Final SKU: <code>{sku}</code></h2>
+          <button onClick={onEmail}>Email to Sales Team</button>
+        </>
+      ) : (
+        <h2>SKU not valid or incomplete.</h2>
+      )}
+    </div>
+  );
+}
+
+export default Summary;
